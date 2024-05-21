70929c4fab716aef0f110e251d4675a8e7200397
GEVIC21 <ndilbedouvic@gmail.com>
2024-05-21 14:32:42 +0000

    Payement

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from charity import app
+from flask import Flask, render_template, redirect, request, url_for
+import stripe
+
+
+public_key = "pk_test_TYooMQauvdEDq54NiTphI7jx"
+stripe.api_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- charity/core/views.py
+++ charity/core/views.py
@@ -1,7 +1,14 @@
 from flask import render_template,request,Blueprint
 from charity.models import BlogPost
+from flask import Flask, render_template, redirect, request, url_for
+import stripe
+
+
+public_key = "pk_test_TYooMQauvdEDq54NiTphI7jx"
+stripe.api_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
 
 core = Blueprint('core',__name__)
+
 
 @core.route('/')
 def index():
@@ -11,7 +18,7 @@ def index():
     '''
     page = request.args.get('page', 1, type=int)
     blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=10)
-    return render_template('index.html',blog_posts=blog_posts)
+    return render_template('index.html',blog_posts=blog_posts,public_key=public_key)
 
 @core.route('/info')
 def info():
@@ -20,3 +27,25 @@ def info():
     contact page. Any page that doesn't really sync with one of the models.
     '''
     return render_template('info.html')
+
+
+@core.route('/thankyou')
+def thankyou():
+   
+    return render_template('thankyou.html')
+
+@core.route('/payement', methods=['GET', 'POST'])
+def payement():
+    customer = stripe.Customer.create(
+        email= request.form["stripeEmail"], 
+        source= request.form['stripeToken']
+        )
+    
+    charge = stripe.Charge.create(
+        customer=customer.id,
+        amount=1000,
+        currency='usd',
+        description='Donation'
+    )
+   
+    return redirect(url_for('core.thankyou'))
